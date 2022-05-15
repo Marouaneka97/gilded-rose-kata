c1f8d2d32ce7b0b6cc90c220639f6d57394d379d
MAROUANE KAOUKAOU <marouane.kaoukaou@gmail.com>
2022-05-15 17:36:45 +0100

    add items and values

--- src/main/java/com/gildedrose/item/CustomisedItemFactory.java
+++ src/main/java/com/gildedrose/item/CustomisedItemFactory.java
@@ -1,5 +1,24 @@
 package com.gildedrose.item;
 
 public class CustomisedItemFactory {
+	
+	public final static String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public final static String BRIE = "Aged Brie";
+    public final static String BACKSTAGE_PASSES_ITEM = "Backstage passes to a TAFKAL80ETC concert";
+    public final static String CONJURED_ITEM = "Conjured";
+    
+    public CustomisedItem customiseItem(Item item) {
+        if (item.name.equals(SULFURAS)) {
+            return new Sulfuras();
+        } else if (item.name.equals(BRIE)) {
+            return new AgedBrie(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES_ITEM)) {
+            return new BackstagePassesItem(item);
+        } else if (item.name.equals(CONJURED_ITEM)) {
+            return new ConjuredItem(item);
+        } else {
+        	return new StandardItem(item);
+        }
+        }
 
 }

fb13c775b20f8805178e5991f731cee155367230
MAROUANE KAOUKAOU <marouane.kaoukaou@gmail.com>
2022-05-15 16:37:03 +0100

    method extractions

--- src/main/java/com/gildedrose/item/AgedBrie.java
+++ src/main/java/com/gildedrose/item/AgedBrie.java
@@ -2,10 +2,23 @@ package com.gildedrose.item;
 
 public class AgedBrie implements CustomisedItem {
 
-	@Override
-	public void updateState() {
-		// TODO Auto-generated method stub
-		
-	}
+	 private final Item item;
+
+	    public AgedBrie(Item item) {
+	        this.item = item;
+	    }
+
+	    public void updateState() {
+	        decreaseSellByDayValueByOne();
+	        increaseQualityByOne();
+	    }
+
+	    private void decreaseSellByDayValueByOne() {
+	        item.sellIn -= 1;
+	    }
+
+	    private void increaseQualityByOne() {
+	        item.quality += 1;
+	    }
 
 }
--- src/main/java/com/gildedrose/item/BackstagePassesItem.java
+++ src/main/java/com/gildedrose/item/BackstagePassesItem.java
@@ -2,10 +2,38 @@ package com.gildedrose.item;
 
 public class BackstagePassesItem implements CustomisedItem {
 
-	@Override
-	public void updateState() {
-		// TODO Auto-generated method stub
-		
-	}
+	 private final Item item;
 
+	    public BackstagePassesItem(Item item) {
+	        this.item = item;
+	    }
+
+	    public void updateState() {
+	        decreaseSellByDayValueByOne();
+	        if (sellByDayValueIsOver(10)) {
+	            increaseQualityBy(1);
+	        } else if (sellByDayValueIsOver(5)) {
+	            increaseQualityBy(2);
+	        } else if (sellByDayValueIsOver(0)) {
+	            increaseQualityBy(3);
+	        } else {
+	            dropQualityToZero();
+	        }
+	    }
+
+	    private void decreaseSellByDayValueByOne() {
+	        item.sellIn -= 1;
+	    }
+
+	    private boolean sellByDayValueIsOver(int dayNumber) {
+	        return item.sellIn > dayNumber;
+	    }
+
+	    private void increaseQualityBy(int qualityValue) {
+	        item.quality += qualityValue;
+	    }
+
+	    private void dropQualityToZero() {
+	        item.quality = 0;
+	    }
 }
